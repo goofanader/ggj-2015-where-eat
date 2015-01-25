c61a7f32a3ce347e914d0b2c4bbd5d370c3a3230
Elliott <Elliott@localhost>
2015-01-25 15:18:56 -0800

    Window Title

--- trunk/conf.lua
+++ trunk/conf.lua
@@ -4,6 +4,7 @@ local WINDOW_HEIGHT = 720
 function love.conf(t)
    t.window.width = WINDOW_WIDTH
    t.window.height = WINDOW_HEIGHT
+   t.window.title = "What Do We Eat for Dinner?"
    --t.modules.joystick = false
    --t.modules.physics = false
    t.version = "0.9.1"
