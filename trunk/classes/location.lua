b856050c92decfe7a0f2426846d64cda5c530c02
Elliott <Elliott@localhost>
2015-01-25 12:42:19 -0800

    Added tentative win/lose logic. Might not work b/c missing an image.

---
+++ trunk/classes/location.lua
@@ -0,0 +1,19 @@
+require 'class'
+
+Location = newclass("Location")
+
+-- location constructor
+function Location:init(name, genre, cost, quality, delivery, closingTime, slogan)
+   self.name = name
+   self.genre = genre
+   self.cost = tonumber(cost)
+   self.quality = tonumber(quality)
+   self.delivery = tonumber(delivery)
+   self.closingTime = tonumber(closingTime)
+   self.slogan = slogan
+end
+
+-- location's pretty print string
+function Location:__tostring()
+   return self.name .. " (\"" .. self.genre .. "\")"
+end
